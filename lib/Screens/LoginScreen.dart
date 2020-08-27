942f357b361528254a55c8f21204b7e8a4f37546
dranzerAvi <aviralhtc2015@gmail.com>
2020-08-28 02:24:20 +0530

    failed attempt at phone login ☹️

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:groceryapp/Classes/Constants.dart';
 import 'package:groceryapp/Classes/CustomIcons.dart';
 import 'package:groceryapp/Screens/RegistrationPage.dart';
+
+import '../Classes/Constants.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,6 +20,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final pHeight = MediaQuery.of(context).size.height;
+    final pWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: kPrimaryColor,
@@ -33,15 +39,18 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               SizedBox(
-                height: pHeight * 0.042,
+                height: pHeight * 0.040,
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/onboarding.png'),
+                child: Image.asset(
+                  'images/onboarding.png',
+                  height: pHeight * 0.35,
+                ),
               ),
-              SizedBox(
-                height: pHeight * 0.01,
-              ),
+              // SizedBox(
+              //   height: pHeight * 0.01,
+              // ),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
@@ -56,17 +65,20 @@ class _LoginPageState extends State<LoginPage> {
                     child: Column(
                       mainAxisSize: MainAxisSize.max,
                       children: <Widget>[
+                        Spacer(),
                         Padding(
-                          padding: const EdgeInsets.only(
-                            left: 32.0,
-                            right: 32,
-                            top: 36,
+                          padding: EdgeInsets.only(
+                            left: pWidth * 0.08,
+                            right: pWidth * 0.08,
+                            top: pHeight * 0.03,
                           ),
                           child: TextFormField(
                             validator: (value) {
+                              if (value.length == 10) _verifyPhoneNumber();
                               if (value.length < 10) {
                                 return 'Invalid phone number';
                               } else {
+                                _verifyPhoneNumber();
                                 return null;
                               }
                             },
@@ -83,10 +95,10 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(
-                            left: 32.0,
-                            right: 32,
-                            top: 15,
+                          padding: EdgeInsets.only(
+                            left: pWidth * 0.08,
+                            right: pWidth * 0.08,
+                            top: pHeight * 0.01,
                           ),
                           child: TextFormField(
                             validator: (value) {
@@ -109,19 +121,21 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(
-                              top: 20, left: 90, right: 90),
+                          padding: EdgeInsets.only(
+                              top: pHeight * 0.02,
+                              left: pWidth * 0.2,
+                              right: pWidth * 0.2),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
                               Container(
-                                height: 50,
-                                width: 50,
+                                height: pHeight * 0.07,
+                                width: pHeight * 0.07,
                                 decoration: BoxDecoration(
                                   border:
                                       Border.all(width: 3, color: Colors.white),
                                   borderRadius: BorderRadius.all(
-                                    Radius.circular(40),
+                                    Radius.circular(pHeight * 0.035),
                                   ),
                                 ),
                                 child: Ink(
@@ -135,18 +149,18 @@ class _LoginPageState extends State<LoginPage> {
                                         size: 20,
                                         color: Colors.white,
                                       ),
-                                      iconSize: 50,
+                                      iconSize: pHeight * 0.07,
                                       onPressed: () {},
                                     )),
                               ),
                               Container(
-                                height: 50,
-                                width: 50,
+                                height: pHeight * 0.07,
+                                width: pHeight * 0.07,
                                 decoration: BoxDecoration(
                                   border:
                                       Border.all(width: 3, color: Colors.white),
                                   borderRadius: BorderRadius.all(
-                                    Radius.circular(40),
+                                    Radius.circular(pHeight * 0.035),
                                   ),
                                 ),
                                 child: Ink(
@@ -160,18 +174,18 @@ class _LoginPageState extends State<LoginPage> {
                                         size: 20,
                                         color: Colors.white,
                                       ),
-                                      iconSize: 50,
+                                      iconSize: pHeight * 0.07,
                                       onPressed: () {},
                                     )),
                               ),
                               Container(
-                                height: 50,
-                                width: 50,
+                                height: pHeight * 0.07,
+                                width: pHeight * 0.07,
                                 decoration: BoxDecoration(
                                   border:
                                       Border.all(width: 3, color: Colors.white),
                                   borderRadius: BorderRadius.all(
-                                    Radius.circular(40),
+                                    Radius.circular(pHeight * 0.035),
                                   ),
                                 ),
                                 child: Ink(
@@ -185,43 +199,51 @@ class _LoginPageState extends State<LoginPage> {
                                         size: 20,
                                         color: Colors.white,
                                       ),
-                                      iconSize: 50,
-                                      onPressed: () {},
+                                      iconSize: pHeight * 0.07,
+                                      onPressed: _verifyPhoneNumber,
                                     )),
                               ),
                             ],
                           ),
                         ),
                         Spacer(),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: FloatingActionButton(
-                                  onPressed: () {
-                                    if (_formKey.currentState.validate()) {
-                                      Navigator.push(
-                                        context,
-                                        CupertinoPageRoute(
-                                          builder: (context) =>
-                                              RegistrationPage(),
-                                        ),
-                                      );
-                                    }
-                                  },
-                                  backgroundColor: kAccentColor,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  right: 8.0, bottom: pHeight * 0.02),
+                              child: InkWell(
+                                onTap: () async {
+                                  if (_formKey.currentState.validate()) {
+                                    await _signInWithPhoneNumber();
+                                    await Navigator.push(
+                                      context,
+                                      CupertinoPageRoute(
+                                        builder: (context) =>
+                                            RegistrationPage(),
+                                      ),
+                                    );
+                                  }
+                                },
+                                child: Container(
+                                  height: pHeight * 0.07,
+                                  width: pHeight * 0.07,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(pHeight * 0.035),
+                                    ),
+                                    color: kAccentColor,
+                                  ),
                                   child: Icon(
                                     Icons.chevron_right,
-                                    size: 40,
+                                    size: pHeight * 0.05,
 //                                color: Color.fromARGB(255, 242, 96, 22),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
@@ -237,5 +259,71 @@ class _LoginPageState extends State<LoginPage> {
 //        child: Icon(Icons.add),
 //      ),
     );
+  }
+
+  String _message, _verificationId;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  // Example code of how to verify phone number
+  void _verifyPhoneNumber() async {
+    setState(() {
+      _message = '';
+    });
+    print('a');
+    PhoneVerificationCompleted verificationCompleted =
+        (PhoneAuthCredential phoneAuthCredential) async {
+      print('b');
+      await _auth.signInWithCredential(phoneAuthCredential);
+      print(
+          "Phone number automatically verified and user signed in: ${phoneAuthCredential}");
+    };
+
+    PhoneVerificationFailed verificationFailed =
+        (FirebaseAuthException authException) {
+      print('c');
+      setState(() {
+        _message =
+            'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}';
+        print(_message);
+      });
+    };
+
+    PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      print('d');
+      print('Please check your phone for the verification code.');
+      _verificationId = verificationId;
+    };
+
+    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      print('e');
+      _verificationId = verificationId;
+    };
+
+    try {
+      print('sd');
+      await _auth.verifyPhoneNumber(
+          phoneNumber: phone.text,
+          timeout: const Duration(seconds: 5),
+          verificationCompleted: verificationCompleted,
+          verificationFailed: verificationFailed,
+          codeSent: codeSent,
+          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+    } catch (e) {
+      print("Failed to Verify Phone Number: ${e}");
+    }
+  }
+
+  void _signInWithPhoneNumber() async {
+    try {
+      final AuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId,
+        smsCode: otp.text,
+      );
+      final User user = (await _auth.signInWithCredential(credential)).user;
+      print("Successfully signed in UID: ${user.uid}");
+    } catch (e) {
+      print(e);
+    }
   }
 }
